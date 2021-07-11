cae9718245aa155b61ae293e591b8f25ed413292
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-11 21:57:08 +0700

    Add files via upload
    apt update && apt upgrade
    git clone https://github.com/MrAdiaraaa-087/TERMINAL-tmpln
    ls
    cd TERMINAL-tmpln
    sh TERMINAL tmln.sh

---
+++ TERMINAL tmln.sh
@@ -0,0 +1,14 @@
+clear
+
+
+figlet TERMINAL | lolcat
+echo
+echo
+echo'       ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤' | lolcat
+echo'       Author: Adiaraaa           [+]' | lolcat
+echo'       Team  : Cyber Attacker [+]' | lolcat
+echo'       WA     : 081546772792 [+]' | lolcat
+echo'       ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤' | lolcat
+echo'====( WELCOME TO TERMUX )==== | lolcat
+echo
+echo
